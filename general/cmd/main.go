f626dc7605c855a720ca2bc053b9819e24b1e92b
leifson <dorianleifson@gmail.com>
2025-03-07 10:26:18 +0800

    up

--- general/cmd/main.go
+++ general/cmd/main.go
@@ -87,7 +87,7 @@ func generateTestTraffic() {
 		conn, err := net.Dial("tcp", "example.com:80")
 		if err == nil {
 			defer conn.Close()
-			for i := 0; i < 5; i++ {
+			for i := 0; i < 5000000; i++ {
 				conn.Write([]byte("GET / HTTP/1.1\r\nHost: example.com\r\n\r\n"))
 				time.Sleep(1 * time.Second)
 			}
@@ -99,7 +99,7 @@ func generateTestTraffic() {
 		addr, _ := net.ResolveUDPAddr("udp4", "8.8.8.8:53")
 		conn, _ := net.DialUDP("udp4", nil, addr)
 		defer conn.Close()
-		for i := 0; i < 5; i++ {
+		for i := 0; i < 5000000; i++ {
 			conn.Write([]byte("test payload"))
 			time.Sleep(1 * time.Second)
 		}
