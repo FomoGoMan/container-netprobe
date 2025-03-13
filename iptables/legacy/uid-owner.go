1d977512292da47b257f0c621e306bcbc9c1f7ed
leifson <dorianleifson@gmail.com>
2025-03-13 21:13:11 +0800

    add debug message

--- iptables/legacy/uid-owner.go
+++ iptables/legacy/uid-owner.go
@@ -133,6 +133,7 @@ func (m *ContainerMonitor) getHostStats() (uint64, error) {
 
 	rules, _ := m.ipt.ListWithCounters(networkTable, "OUTPUT")
 	for _, rule := range rules {
+		fmt.Printf("Rule: %s\n", rule)
 		if strings.Contains(rule, fmt.Sprintf("--uid-owner %v", m.uid)) {
 			fields := strings.Fields(rule)
 			if len(fields) >= 9 {
